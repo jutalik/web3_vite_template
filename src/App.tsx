d9b1945b680401ce5bae80726318ce5c19a20a2c
jutalik <50254404+jutalik@users.noreply.github.com>
2024-03-17 14:15:38 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './connect.ts'
+import Caver from 'caver-js'
+import axios from 'axios'
+import {tokenAddr,tokenABI} from './assets/constants'
 
-
-
+// const caver = new Caver('https://api.baobab.klaytn.net:8651/')
+const PER = new caver.klay.Contract(tokenABI, tokenAddr);
+const apiRoot = axios.create({
+  baseURL: 'http://localhost:8080'
+})
 function App() {
   
 
   const [account, setAccount] = useState('')
-
+  const [_caver, setCaver] = useState<Caver>()
   const connectKaikas = async () => {
     // connect.ts 참고
     if (window.klaytn) {
       try {
         const accounts = await window.klaytn.enable();
+        setCaver(new Caver(window.klaytn))
         setAccount(accounts[0]);
+        
       } catch (error) {
         console.log(error);
       }
     }
   }
 
+  const sign = async () => {
+
+  const caver = new Caver(window.klaytn)
+  const data = PER.methods.transfer('0xaAFD419b0d608abE68E1485BbF857Fb9919FCDF9', '1000000000000000000').encodeABI();
+
+  const {rawTransaction} = await caver.klay.signTransaction(
+    {
+      type: "FEE_DELEGATED_SMART_CONTRACT_EXECUTION",
+      from: account,
+      to: tokenAddr,
+      data: data,
+      gas: "0x4bfd200",
+    }
+  );
+  
+  console.log(rawTransaction);
+  
+  }
+ 
+  
 
   return (
     <>
@@ -38,7 +66,13 @@ function App() {
       <div className="card">
         <button onClick={() => connectKaikas()}>
           connect Kaikas
+        </button>
         
+        <p>
+        connect is {account}
+        </p>
+        <button onClick={() => sign()}>
+        sign
         </button>
         
         <p>
